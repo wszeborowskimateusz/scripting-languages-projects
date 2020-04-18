686cf76c4a98e19dfa04c7ed25f0a5896430719e
Mateusz <wszeborowskimateusz@gmail.com>
2020-04-18 14:46:32 +0200

    Adding project 3

---
+++ Project3/fileExplorerGTK.py
@@ -0,0 +1,405 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import stat
+import platform
+import subprocess
+import shutil
+from datetime import datetime
+from pwd import getpwuid
+import time
+from gi import require_version
+
+require_version( 'Gtk', '3.0' )
+from gi.repository import Gtk, Gdk, Gio
+from gi.repository.GdkPixbuf import Pixbuf
+
+HELP_GUIDE = '''
+Możliwe funkcje w programie:
+- Menu Folder - pozwala na otworzenie nowego folderu jako folder root
+- Po kliknięciu w wybrany nagłówek, zawartość zostanie posortowana względem podanej wartości
+- Zaznaczenie pliku tekstowego pozwala na wyświetlenie jego treści
+- Menu pod prawym przyciskiem myszy:
+    - Otwórz - otwiera plik/folder w okienku systemowym
+    - Usuń - usuwa plik/folder, przed usunięciem pyta o potwierdzenie
+    - Właściwości - wyświetla właściwości wybranego elementu: typ, rozmiar, datę edycji, właściciela oraz uprawnienia 
+'''
+
+AUTHOR_INFO = '''
+Program stworzony na potrzeby przedmiotu Języki Programowania 2020
+Autor: Mateusz Wszeborowski 165562
+'''
+
+CANNOT_OPEN_FILE_DISCLAIMER = 'Nie udało się otworzyć pliku. Najprawdopodobniej nie jest to plik tekstowy.'
+EMPTY_FILE_DISCLAIMER = 'Wybrany plik jest pusty.'
+NO_PERMISSION_FILE_DISCLAIMER = 'Nie masz uprawnień aby otworzyć ten plik.'
+NO_PERMISSI__on_file_delete_DISCLAIMER = 'Nie masz uprawnień aby usunąć ten plik.'
+NO_PROPERTIES_AVAILABLE = 'Nie udało się wczytać właściwości pliku.'
+
+def show_info_popup(window, title, text):
+    dialog = Gtk.MessageDialog(
+        window,
+        0,
+        Gtk.MessageType.INFO,
+        Gtk.ButtonsType.OK,
+        title,
+    )
+    dialog.format_secondary_text(text)
+    dialog.run()
+    dialog.destroy()
+
+def show_yes_no_popup(window, title, text, onYes):
+    dialog = Gtk.MessageDialog(
+        window,
+        0,
+        Gtk.MessageType.QUESTION,
+        Gtk.ButtonsType.YES_NO,
+        title,
+    )
+    dialog.format_secondary_text(text)
+    response = dialog.run()
+    if response == Gtk.ResponseType.YES:
+        onYes()
+    dialog.destroy()
+
+class FilesTree():
+    def __init__(self, text_buffer, window):
+        self.__text_buffer = text_buffer
+        self.__window = window
+
+    def __populate_file_system_tree(self, tree_store, path, parent=None):
+        itemCounter = 0
+        try:
+            for item in os.listdir(path):
+                item_full_name = os.path.join(path, item)
+                item_is_folder = os.path.isdir(item_full_name)
+                item_size = ""
+                item_size_in_bytes = 0
+                if not item_is_folder:
+                    try:
+                        item_size_in_bytes = os.path.getsize(item_full_name)
+                        item_size = self.__convert_file_size_to_string(item_size_in_bytes)
+                    except PermissionError:
+                        pass
+                timestamp = os.path.getmtime(item_full_name)
+                date_time = datetime.fromtimestamp(timestamp)
+                item_modified_date = date_time.strftime("%d.%m.%Y %H:%M")
+                current_iter = tree_store.append(parent, 
+                    [item, item_full_name, item_size, item_modified_date, item_size_in_bytes, timestamp])
+
+                if item_is_folder: tree_store.append(
+                    current_iter, 
+                    [None, None, None, None, None, None]
+                )
+
+                itemCounter += 1
+     
+            if itemCounter < 1: tree_store.append(parent, [None, None, None, None, None, None])
+        except PermissionError:
+            pass
+
+    def __refresh_tree(self):
+        self.fileSystemTreeView.set_model(self.get_new_tree_model(self.__path))
+
+    def __convert_file_size_to_string(self, size_in_bytes):
+        if size_in_bytes < 1024:
+            return str(size_in_bytes) + ' bytes'
+        if size_in_bytes < 1024 * 1024:
+            return str(round(size_in_bytes / 1024.0, 2)) + ' KB'
+        if size_in_bytes < 1024 * 1024 * 1024:
+            return str(round(size_in_bytes / (1024.0 * 1024.0), 2)) + ' MB'
+        return str(round(size_in_bytes / (1024.0 * 1024.0 * 1024.0), 2)) + ' GB'
+
+    def __on_row_expanded(self, tree_view, tree_iter, _):
+        tree_store = tree_view.get_model()
+        # Full path
+        new_path = tree_store.get_value(tree_iter, 1)
+
+        self.__populate_file_system_tree(tree_store, new_path, tree_iter)
+        # remove the first child (dummy node)
+        tree_store.remove(tree_store.iter_children(tree_iter))
+
+    def __on_row_collapsed(self, tree_view, tree_iter, _):
+        tree_store = tree_view.get_model()
+
+        current_child_iter = tree_store.iter_children(tree_iter)
+
+        while current_child_iter:
+            tree_store.remove(current_child_iter)
+            current_child_iter = tree_store.iter_children(tree_iter)
+
+        tree_store.append(tree_iter, [None, None, None, None])
+
+    def __on_row_selected(self, tree_view, tree_path, view_column):
+        tree_store = tree_view.get_model()
+
+        path = tree_store.get_value(tree_store.get_iter(tree_path), 1)
+        if os.path.isfile(path):
+            try:
+                with open(path, 'r') as myfile:
+                    data = myfile.read()
+                    if not data or data == '' or data.isspace():
+                        data = EMPTY_FILE_DISCLAIMER
+                    self.__text_buffer.set_text(data)
+            # File is not a text file
+            except UnicodeDecodeError:
+                self.__text_buffer.set_text(CANNOT_OPEN_FILE_DISCLAIMER)
+            except PermissionError:
+                self.__text_buffer.set_text(NO_PERMISSION_FILE_DISCLAIMER)
+
+    def __on_right_click(self, widget, event):
+        # right mouse click
+        if event.button == 3:
+            path, _, _, _ = widget.get_path_at_pos(event.x, event.y)
+            if path != None:
+                tree_store = widget.get_model()
+                tree_iter = tree_store.get_iter(path)
+                new_path = tree_store.get_value(tree_iter, 1)
+                self.__append_right_click_menu(widget, new_path)
+
+    def __append_right_click_menu(self, widget, new_path):
+        self.__menu = Gtk.Menu()
+
+        self.__open_menu = Gtk.MenuItem.new_with_label("Otwórz")
+        self.__delete_menu = Gtk.MenuItem.new_with_label("Usuń")
+        self.__properties_menu = Gtk.MenuItem.new_with_label("Właściwości")
+
+        self.__open_menu.connect('activate', self.__on_file_open(new_path))
+        self.__delete_menu.connect('activate', self.__on_file_delete(new_path))
+        self.__properties_menu.connect('activate', self.__on_file_properties(new_path))
+
+        self.__menu.append(self.__open_menu)
+        self.__menu.append(self.__delete_menu)
+        self.__menu.append(self.__properties_menu)
+        self.__menu.show_all()
+        self.__menu.popup_at_pointer()
+        
+    def __on_file_open(self, path):
+        def open_file_internal(e):
+            if platform.system() == "Windows":
+                os.startfile(path)
+            elif platform.system() == "Darwin":
+                subprocess.Popen(["open", path])
+            else:
+                subprocess.Popen(["xdg-open", path])
+        
+        return open_file_internal
+
+    def __delete_file(self, path):
+        try:
+            if os.path.isfile(path):
+                os.remove(path)
+            elif os.path.isdir(path):
+                shutil.rmtree(path, ignore_errors=True)
+            self.__refresh_tree()
+        except PermissionError:
+            show_info_popup(self.__window, "Brak uprawnień", "Nie ma uprawnień aby usunąć ten plik")
+
+    def __on_file_delete(self, path):
+        def delete_file_internal(e):
+            quit_msg = f"Czy jesteś pewien, że chesz usunąć: {path}?"
+            show_yes_no_popup(
+                self.__window, 
+                'Czy jesteś pewien?', 
+                quit_msg, 
+                lambda: self.__delete_file(path)
+            )
+        
+        return delete_file_internal
+
+    def __on_file_properties(self, path):
+        def file_properties_internal(e):
+            show_info_popup(self.__window, "Właściwości", self.__build_properties_text(path))
+        
+        return file_properties_internal
+
+    def __build_properties_text(self, path):
+        try:
+            text = f'Nazwa: {path}\n'
+            if os.path.isfile(path):
+                text += 'Typ: Plik\n'
+            elif os.path.isdir(path):
+                text += 'Typ: Folder\n'
+            file_stats = os.stat(path)
+            text += f'Rozmiar: {os.path.getsize(path)} bajtów\n'
+            text += f'Data edycji: {time.ctime(os.path.getmtime(path))}\n'
+            text += f'Właściciel: {getpwuid(file_stats.st_uid).pw_name}\n'
+            text += f'Uprawnienia: {stat.filemode(file_stats.st_mode)}'
+            return text
+        except:
+            return NO_PROPERTIES_AVAILABLE
+
+    def get_new_tree_view(self, path='/'):
+        self.fileSystemtree_store = self.get_new_tree_model(path)
+
+        self.fileSystemTreeView = Gtk.TreeView(self.fileSystemtree_store)
+        self.fileSystemTreeView.set_activate_on_single_click(True)
+  
+        # Name column
+        treeViewCol = Gtk.TreeViewColumn("Nazwa")
+        colCellText = Gtk.CellRendererText()
+        treeViewCol.pack_start(colCellText, True)
+        treeViewCol.add_attribute(colCellText, "text", 0)
+        treeViewCol.set_sort_column_id(0)
+        treeViewCol.set_sort_indicator(True)
+        self.fileSystemTreeView.append_column(treeViewCol)
+
+        # size column
+        treeViewCol = Gtk.TreeViewColumn("Rozmiar")
+        colCellText = Gtk.CellRendererText()
+        treeViewCol.pack_start(colCellText, True)
+        treeViewCol.add_attribute(colCellText, "text", 2)
+        treeViewCol.set_sort_column_id(4)
+        treeViewCol.set_sort_indicator(True)
+        self.fileSystemTreeView.append_column(treeViewCol)
+
+        # last modified column
+        treeViewCol = Gtk.TreeViewColumn("Data edycji")
+        colCellText = Gtk.CellRendererText()
+        treeViewCol.pack_start(colCellText, True)
+        treeViewCol.add_attribute(colCellText, "text", 3)
+        treeViewCol.set_sort_column_id(5)
+        treeViewCol.set_sort_indicator(True)
+        self.fileSystemTreeView.append_column(treeViewCol)
+
+        self.fileSystemTreeView.connect("row-expanded", self.__on_row_expanded)
+        self.fileSystemTreeView.connect("row-collapsed", self.__on_row_collapsed)
+        self.fileSystemTreeView.connect("row-activated", self.__on_row_selected)
+        self.fileSystemTreeView.connect("button_press_event", self.__on_right_click)
+
+        return self.fileSystemTreeView
+
+    def get_new_tree_model(self, path):
+        self.__path = path
+        # Displayed name, full path, size, date of last edit date, size in bytes, timestamp
+        # The last two columns are just for sorting purposes
+        self.fileSystemtree_store = Gtk.TreeStore(str, str, str, str, int, int)
+
+        self.__populate_file_system_tree(self.fileSystemtree_store, path)
+
+        return self.fileSystemtree_store
+
+class MenuWindow(Gtk.Window):
+    __MENU_BAR = """
+        <ui>
+        <menubar name='MenuBar'>
+            <menu action='FileMenu'>
+            <menuitem action='FileOpenFolder' />
+            <separator />
+            </menu>
+            <menu action='HelpMenu'>
+            <menuitem action='HelpMenuTutorial' />
+            </menu>
+            <menu action='AboutMenu'>
+            <menuitem action='AboutMenuAuthor' />
+            </menu>
+        </menubar>
+        </ui>
+    """
+
+    def __init__(self):
+        super(MenuWindow, self).__init__()
+
+        self.set_title("Eksplorator plików")
+        self.set_default_size(1000, 600)
+
+        action_group = Gtk.ActionGroup("my_actions")
+
+        self.__add_file_menu_actions(action_group)
+        self.__add_edit_menu_actions(action_group)
+        self.__add_choices_menu_actions(action_group)
+
+        uimanager = self.__create_ui_manager()
+        uimanager.insert_action_group(action_group)
+
+        menubar = uimanager.get_widget("/MenuBar")
+        self.__set_layout(menubar)
+        
+    def __set_layout(self, menubar):
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box.pack_start(menubar, False, False, 0)
+
+        self.textview = Gtk.TextView()
+        self.textview.set_editable(False)
+        self.textbuffer = self.textview.get_buffer()
+        self.textbuffer.set_text('')
+
+        scrolled_window_for_text = Gtk.ScrolledWindow()
+        scrolled_window_for_text.set_border_width(10)
+        scrolled_window_for_text.add(self.textview)
+
+        self.tree = FilesTree(self.textbuffer, self)
+        self.treeView = self.tree.get_new_tree_view()
+
+        scrolled_window_for_tree = Gtk.ScrolledWindow()
+        scrolled_window_for_tree.set_border_width(10)
+        scrolled_window_for_tree.add(self.treeView)
+
+        horizontal_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        horizontal_box.pack_start(scrolled_window_for_tree, True, True, 0)
+        horizontal_box.pack_start(scrolled_window_for_text, True, True, 0)
+
+        box.pack_start(horizontal_box, True, True, 0)
+
+        self.add(box)
+
+    def __add_file_menu_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("FileMenu", None, "Folder"),
+                ("FileOpenFolder", Gtk.STOCK_OPEN, "Otwórz", None, None, self.__on_menu_openFolder),
+            ]
+        )
+
+    def __add_edit_menu_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("HelpMenu", None, "Pomoc"),
+                ("HelpMenuTutorial", Gtk.STOCK_HELP, "Otwórz przewodnik", "<control>H", None, self.__on_menu_tutorial),
+            ]
+        )
+
+    def __add_choices_menu_actions(self, action_group):
+        action_group.add_actions(
+            [
+                ("AboutMenu", None, "O autorze"),
+                ("AboutMenuAuthor", Gtk.STOCK_ABOUT, "Informacje o autorze", "<control>I", None, self.__on_menu_author_info),
+            ]
+        )
+
+    def __create_ui_manager(self):
+        uimanager = Gtk.UIManager()
+
+        uimanager.add_ui_from_string(MenuWindow.__MENU_BAR)
+
+        accelgroup = uimanager.get_accel_group()
+        self.add_accel_group(accelgroup)
+        return uimanager
+
+    def __on_menu_openFolder(self, widget):
+        dialog = Gtk.FileChooserDialog(
+            "Wybierz folder",
+            self,
+            Gtk.FileChooserAction.SELECT_FOLDER,
+            ("Anuluj", Gtk.ResponseType.CANCEL, "Wybierz", Gtk.ResponseType.OK),
+        )
+        dialog.set_default_size(300, 300)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            self.treeView.set_model(self.tree.get_new_tree_model(dialog.get_filename()))
+        dialog.destroy()
+
+    def __on_menu_tutorial(self, widget):
+        show_info_popup(self, "Pomoc", HELP_GUIDE)
+
+    def __on_menu_author_info(self, widget):
+        show_info_popup(self, "O autorze", AUTHOR_INFO)
+
+
+window = MenuWindow()
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+Gtk.main()
---
+++ Project3/fileExplorerQT.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import os
+import stat
+import platform
+import subprocess
+import shutil
+import time
+from pwd import getpwuid
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+HELP_GUIDE = '''
+Możliwe funkcje w programie:
+- Menu Folder - pozwala na otworzenie nowego folderu jako folder root
+- Po kliknięciu w wybrany nagłówek, zawartość zostanie posortowana względem podanej wartości
+- Podwójne kliknięcie danego pliku/folderu pozwala na jeg edycję
+- Zaznaczenie pliku tekstowego pozwala na wyświetlenie jego treści
+- Menu pod prawym przyciskiem myszy:
+    - Otwórz - otwiera plik/folder w okienku systemowym
+    - Usuń - usuwa plik/folder, przed usunięciem pyta o potwierdzenie
+    - Właściwości - wyświetla właściwości wybranego elementu: typ, rozmiar, datę edycji, właściciela oraz uprawnienia 
+'''
+
+AUTHOR_INFO = '''
+Program stworzony na potrzeby przedmiotu Języki Programowania 2020
+Autor: Mateusz Wszeborowski 165562
+'''
+
+CANNOT_OPEN_FILE_DISCLAIMER = 'Nie udało się otworzyć pliku. Najprawdopodobniej nie jest to plik tekstowy.'
+EMPTY_FILE_DISCLAIMER = 'Wybrany plik jest pusty.'
+NO_PERMISSION_FILE_DISCLAIMER = 'Nie masz uprawnień aby otworzyć ten plik.'
+NO_PERMISSION_FILE_DELETE_DISCLAIMER = 'Nie masz uprawnień aby usunąć ten plik.'
+NO_PROPERTIES_AVAILABLE = 'Nie udało się wczytać właściwości pliku.'
+
+class PolishQFileSystemModel(QFileSystemModel):
+    def headerData(self, section, orientation, role):
+        if section == 0 and role == Qt.DisplayRole:
+            return "Nazwa"
+        elif section == 1 and role == Qt.DisplayRole:
+            return "Rozmiar"
+        elif section == 3 and role == Qt.DisplayRole:
+            return "Data edycji"
+        else:
+            return super(PolishQFileSystemModel, self).headerData(section, orientation, role)
+
+class Browser(QWidget):
+    def __init__(self, file_viewer):
+        super(Browser, self).__init__()
+
+        self.file_viewer = file_viewer
+
+        self.treeView = QTreeView()
+        self.fileSystemModel = PolishQFileSystemModel(self.treeView)
+        self.fileSystemModel.setReadOnly(False)
+        
+        root = self.fileSystemModel.setRootPath('/')
+        self.treeView.setModel(self.fileSystemModel)
+        self.treeView.setRootIndex(root)
+        self.treeView.setSortingEnabled(True)
+        self.treeView.setExpandsOnDoubleClick(True)
+        self.treeView.clicked.connect(self.__single_click)
+        self.treeView.hideColumn(2)
+
+        self.treeView.setContextMenuPolicy(Qt.CustomContextMenu)  
+        self.treeView.customContextMenuRequested.connect(self.__create_right_click_menu)  
+
+        Layout = QVBoxLayout(self)
+        Layout.addWidget(self.treeView)
+        self.setLayout(Layout)
+
+    def __open_file_name_dialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName = str(QFileDialog.getExistingDirectory(self,"Wybierz folder", options=options))
+        if fileName:
+            return fileName
+        return None
+
+    def set_new_directory(self):
+        new_file_name = self.__open_file_name_dialog()
+        if new_file_name:
+            root = self.fileSystemModel.setRootPath(new_file_name)
+            self.treeView.setModel(self.fileSystemModel)
+            self.treeView.setRootIndex(root)
+
+    def __open_system_file_browser(self, index):
+        path = self.sender().model().filePath(index)
+        self.__open_file(path)
+
+    def __delete_file(self, index):
+        path = self.sender().model().filePath(index)
+        quit_msg = f"Czy jesteś pewien, że chesz usunąć: {path}?"
+        reply = QMessageBox.question(self, 'Czy jesteś pewien?', 
+                        quit_msg, QMessageBox.Yes, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            try:
+                if os.path.isfile(path):
+                    os.remove(path)
+                elif os.path.isdir(path):
+                    shutil.rmtree(path, ignore_errors=True) 
+            except PermissionError:
+                self.__show_no_permission_popup()
+
+    def __show_no_permission_popup(self):
+        self.no_permision_popup = QMessageBox()
+        self.no_permision_popup.setWindowTitle("Brak uprawnień")
+        self.no_permision_popup.setText(NO_PERMISSION_FILE_DELETE_DISCLAIMER)
+        x = self.no_permision_popup.exec_() 
+
+    def __create_right_click_menu(self, event):
+        self.right_click_menu = QMenu(self.treeView)
+        open_action = self.right_click_menu.addAction("Otwórz")
+        delete_action = self.right_click_menu.addAction("Usuń")
+        properties_action = self.right_click_menu.addAction("Właściwości")
+        picked_action = self.right_click_menu.exec_(self.treeView.mapToGlobal(event))
+        if picked_action:
+            index = self.treeView.indexAt(event)
+            if index.isValid():
+                if picked_action == open_action:
+                    self.__open_system_file_browser(index)
+                elif picked_action == delete_action:
+                    self.__delete_file(index)
+                elif picked_action == properties_action:
+                    self.__show_file_properties(index)
+
+    def __single_click(self, index):
+        path = self.sender().model().filePath(index)
+        if os.path.isfile(path):
+            try:
+                with open(path, 'r') as myfile:
+                    data = myfile.read()
+                    if not data or data == '' or data.isspace():
+                        data = EMPTY_FILE_DISCLAIMER
+                    self.file_viewer.setPlainText(data)
+            # File is not a text fileopen_file
+            except UnicodeDecodeError:
+                self.file_viewer.setPlainText(CANNOT_OPEN_FILE_DISCLAIMER)
+            except PermissionError:
+                self.file_viewer.setPlainText(NO_PERMISSION_FILE_DISCLAIMER)
+
+    def __open_file(self, path):
+        if platform.system() == "Windows":
+            os.startfile(path)
+        elif platform.system() == "Darwin":
+            subprocess.Popen(["open", path])
+        else:
+            subprocess.Popen(["xdg-open", path])
+
+    def __show_file_properties(self, index):
+        path = self.sender().model().filePath(index)
+        self.file_properties_popup = QMessageBox()
+        self.file_properties_popup.setWindowTitle("Właściwości")
+        self.file_properties_popup.setText(self.__build_properties_text(path))
+        x = self.file_properties_popup.exec_() 
+
+    def __build_properties_text(self, path):
+        try:
+            text = f'Nazwa: {path}\n'
+            if os.path.isfile(path):
+                text += 'Typ: Plik\n'
+            elif os.path.isdir(path):
+                text += 'Typ: Folder\n'
+            file_stats = os.stat(path)
+            text += f'Rozmiar: {os.path.getsize(path)} bajtów\n'
+            text += f'Data edycji: {time.ctime(os.path.getmtime(path))}\n'
+            text += f'Właściciel: {getpwuid(file_stats.st_uid).pw_name}\n'
+            text += f'Uprawnienia: {stat.filemode(file_stats.st_mode)}'
+            return text
+        except:
+            return NO_PROPERTIES_AVAILABLE
+
+class MainLayout(QWidget):
+    def __init__(self, left_widget, right_widget):
+        super(MainLayout, self).__init__()
+
+        hbox = QHBoxLayout(self)
+        
+        splitter = QSplitter(Qt.Horizontal)
+        splitter.addWidget(left_widget)
+        splitter.addWidget(right_widget)
+        splitter.setStretchFactor(1, 1)
+        splitter.setSizes([400, 500])
+        hbox.addWidget(splitter)
+        self.setLayout(hbox)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.resize(1000, 600)
+        self.setWindowTitle("Eksplorator plików")
+
+        self.file_viewer = QPlainTextEdit()
+        self.file_viewer.setReadOnly(True)
+        self.tree_widget = Browser(self.file_viewer)
+
+        self.main_layout = MainLayout(self.tree_widget, self.file_viewer)
+        self.setCentralWidget(self.main_layout)
+
+        self.statusBar()
+        self.__file_menu()
+        self.__help_menu()
+        self.__about_menu()
+
+
+    def __file_menu(self):
+        open_folder_menu = QAction("Otwórz", self)
+        open_folder_menu.setShortcut("Ctrl+O")
+        open_folder_menu.setStatusTip('Otwórz folder')
+        open_folder_menu.triggered.connect(self.tree_widget.set_new_directory)
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&Folder')
+        fileMenu.addAction(open_folder_menu)
+
+    def __help_menu(self):
+        help_folder_menu = QAction("Otwórz przewodnik", self)
+        help_folder_menu.setShortcut("Ctrl+H")
+        help_folder_menu.setStatusTip('Przewodnik')
+        help_folder_menu.triggered.connect(self.__open_guide)
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('&Pomoc')
+        fileMenu.addAction(help_folder_menu)
+
+    def __open_guide(self):
+        self.help_guide = QMessageBox()
+        self.help_guide.setWindowTitle("Pomoc")
+        self.help_guide.setText(HELP_GUIDE)
+        x = self.help_guide.exec_() 
+
+    def __about_menu(self):
+        about_menu = QAction("Informacje o autorze", self)
+        about_menu.setShortcut("Ctrl+I")
+        about_menu.setStatusTip('Informacje o autorze')
+        about_menu.triggered.connect(self.__open_about_info)
+
+        mainMenu = self.menuBar()
+        about = mainMenu.addMenu('&O autorze')
+        about.addAction(about_menu)
+
+    def __open_about_info(self):
+        self.about_info = QMessageBox()
+        self.about_info.setWindowTitle("O autorze")
+        self.about_info.setText(AUTHOR_INFO)
+        x = self.about_info.exec_() 
+
+    
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
