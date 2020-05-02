6195aa6ad0053a44887f50428ea381a3bbc2f4aa
Mateusz <wszeborowskimateusz@gmail.com>
2020-05-02 18:21:21 +0200

    Adding article uis

--- Project4/blog/articles/urls.py
+++ Project4/blog/articles/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
+    path('', views.article_list, name='list'),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name='detail')
 ]
--- Project4/blog/articles/views.py
+++ Project4/blog/articles/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Article
 
 def article_list(request):
-    return render(request, "articles/article_list.html")
+    articles = Article.objects.all().order_by('timestamp')
+    return render(request, "articles/article_list.html", {'articles': articles})
+
+def article_detail(request, slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, "articles/article_detail.html", {'article': article})
